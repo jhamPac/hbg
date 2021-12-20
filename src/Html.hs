4eb91b5ff463c575170f72f0e9704d270371cda4
jhamPac <cloud@kevia.me>
2021-12-19 17:08:14 -0700

    feat: guarding with types

--- src/Html.hs
+++ src/Html.hs
@@ -6,6 +6,10 @@ myhtml = html' (body' "Application hbg")
 makeHtml :: String -> String -> String
 makeHtml title content = html' (head' (title' title) <> body' content)
 
+newtype Html = Html String
+
+newtype Structure = Structure String
+
 el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
@@ -22,8 +26,16 @@ head' = el "head"
 title' :: String -> String
 title' = el "title"
 
-p' :: String -> String
-p' = el "p"
+p' :: String -> Structure
+p' = Structure . el "p"
 
 h1 :: String -> String
 h1 = el "h1"
+
+append' :: Structure -> Structure -> Structure
+append' (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render html =
+    case html of
+        Html str -> str
