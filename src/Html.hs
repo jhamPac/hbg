f47d6fc2dd56bbb04ff10e15b68563db202c1078
jhamPac <cloud@kevia.me>
2022-01-03 19:12:31 -0700

    feat: update internal functions to use new data type

--- src/Html.hs
+++ src/Html.hs
@@ -25,8 +25,13 @@ el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html' :: String -> String
-html' = el "html"
+html' :: Title -> Structure -> Html
+html' title content =
+    Html
+        ( el "html"
+            (el "head" (el "title" title))
+            <> el "body" (getStructureString content)
+        )
 
 body' :: String -> String
 body' = el "body"
@@ -45,6 +50,11 @@ h1 = el "h1"
 
 append' :: Structure -> Structure -> Structure
 append' (Structure a) (Structure b) = Structure (a <> b)
+
+getStructureString :: Structure -> String
+getStructureString content =
+    case content of
+        Structure str -> str
 
 render :: Html -> String
 render html =
