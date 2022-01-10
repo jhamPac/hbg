9efb093b6f61b1312a867bfc975137ceb51cb921
jhamPac <cloud@kevia.me>
2022-01-09 20:52:49 -0700

    refactor: implement do notion in the main app

--- app/Main.hs
+++ app/Main.hs
@@ -3,49 +3,50 @@ module Main where
 import           Convert            (convert)
 import qualified Html
 import qualified Markup
-import           System.Directory
-import           System.Environment
+import           System.Directory   (doesFileExist)
+import           System.Environment (getArgs)
 import           System.IO          ()
 
 main :: IO ()
-main =
-    getArgs >>= \args ->
-        case args of
+main = do
+    args <- getArgs
+    case args of
+    -- No program arguments: read from stdin
+        [] -> do
+            content <- getContents
+            putStrLn (process "Empty title" content)
 
-            -- No program arguments: read from stdin
-            [] ->
-                getContents >>= \content ->
-                    putStrLn (process "Empty title" content)
+    -- With input and output file paths as program args
+        [input, output] -> do
+            content <- readFile input
+            exists <- doesFileExist output
+            let
+                writeResult = writeFile output (process input content)
+            if exists
+                then whenIO confirm writeResult
+                else writeResult
 
-            -- With input and output args
-            [input, output] ->
-                readFile input >>= \content ->
-                    doesFileExist output >>= \exits ->
-                        let
-                            writeResult = writeFile output (process input content)
-                        in
-                            if exits
-                                then whenIO confirm writeResult
-                                else writeResult
+        _ ->
+            putStrLn "Usage: exe [-- <input-file> <output-file>]"
 
-            _ ->
-                putStrLn "Unsage: -- <input-file> <output-file>"
+
+
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action =
-    cond >>= \result ->
-        if result
-            then action
-            else pure ()
+whenIO cond action = do
+    result <- cond
+    if result
+        then action
+        else pure ()
 
-confirm =
-    putStrLn "Are you sure? (y/n)" *>
-        getLine >>= \answer ->
-            case answer of
-                "y" -> pure True
-                "n" -> pure False
-                _ -> putStrLn "Invalid response. use y or n" *>
-                    confirm
+confirm = do
+    putStrLn "Are you sure? (y/n)"
+    answer <- getLine
+    case answer of
+        "y" -> pure True
+        "n" -> pure False
+        _ -> putStrLn "Invalid response. use y or n" *>
+            confirm
