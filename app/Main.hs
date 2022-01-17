ac020f1365e056b37a4d51ddea76a2f9fabba47d
jhamPac <cloud@kevia.me>
2022-01-17 11:58:08 -0700

    feat: complete hbg main

--- app/Main.hs
+++ app/Main.hs
@@ -1,53 +1,53 @@
 module Main where
 
-import           Convert            (convert)
-import qualified Html
-import qualified Markup
+import qualified Blog
+import           OptParse
+
 import           System.Directory   (doesFileExist)
 import           System.Environment (getArgs)
+import           System.Exit        (exitFailure)
 import           System.IO          ()
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-    -- No program arguments: read from stdin
-        [] -> do
-            content <- getContents
-            putStrLn (process "Empty title" content)
+    options <- parse
+    case options of
+        ConvertDir input output ->
+            Blog.convertDirectory input output
 
-    -- With input and output file paths as program args
-        [input, output] -> do
-            content <- readFile input
-            exists <- doesFileExist output
-            let
-                writeResult = writeFile output (process input content)
-            if exists
-                then whenIO confirm writeResult
-                else writeResult
+        ConvertSingle input output -> do
+            (title, inputHandle) <-
+                case input of
+                    Stdin -> pure ("", stdin)
+                    InputFile file ->
+                        (,) file <$> openFile file ReadMode
 
-        _ ->
-            putStrLn "Usage: exe [-- <input-file> <output-file>]"
+            outputHandle <-
+                case output of
+                    Stdout -> pure stdout
+                    OutputFile file -> do
+                        exists <- doesFileExist file
+                        shouldOpenFile <-
+                            if exists
+                                then confirm
+                                else pure True
+                        if shouldOpenFile
+                            then
+                                openFile file WriteMode
+                            else
+                                exitFailure
 
+            Blog.convertSingle title inputHandle outputHandle
+            hClose inputHandle
+            hClose outputHandle
 
-
-
-process :: Html.Title -> String -> String
-process title = Html.render . convert title . Markup.parse
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = do
-    result <- cond
-    if result
-        then action
-        else pure ()
 
 confirm :: IO Bool
-confirm = do
-    putStrLn "Are you sure? (y/n)"
-    answer <- getLine
-    case answer of
-        "y" -> pure True
-        "n" -> pure False
-        _ -> putStrLn "Invalid response. use y or n" *>
-            confirm
+confirm =
+    putStrLn "Are you sure? (y/n)" *>
+        getLine >>= \answer ->
+            case answer of
+                "y" -> pure True
+                "n" -> pure False
+                _   -> putStrLn "Invalid response" *> confirm
+
